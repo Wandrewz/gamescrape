d4ec9045f8bf97a442ab052243a49da0ffd318f3
Andrew Taavola <ataavola@asu.edu>
2020-04-05 22:18:12 -0400

    started django app

--- amazonapi.py
+++ amazonapi.py
@@ -6,7 +6,7 @@ querystring = {"country":"US","asin":"B01MS6MO77"}
 
 headers = {
     'x-rapidapi-host': "amazon-products1.p.rapidapi.com",
-    'x-rapidapi-key': "cbe7bbccbamsh155304094958281p187d2fjsn2783009cf0b4"
+    'x-rapidapi-key': "1167c5a233msh60ccafe772449f8p1bc540jsn304b2d05e0ba"
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
---
+++ gamescrapesite/gamescrape/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GamescrapeConfig(AppConfig):
+    name = 'gamescrape'
