44af5c537b8f7c9d196ae9126bb936ff6332e914
Andrew Taavola <ataavola@asu.edu>
2020-05-03 14:12:25 -0400

    added more games

--- gamescrapesite/gamescrape/models.py
+++ gamescrapesite/gamescrape/models.py
@@ -5,13 +5,13 @@ class Game(models.Model):
     slug = models.SlugField()
     cover = models.ImageField(upload_to='images/')
     amazonurl = models.URLField(max_length=200)
-    amazonprice = models.DecimalField(max_digits=5, decimal_places=2)
+    amazonprice = models.DecimalField(max_digits=5, decimal_places=2, default=100.00)
     walmarturl = models.URLField(max_length=200)
-    walmartprice = models.DecimalField(max_digits=5, decimal_places=2)
-    lowestprice = models.DecimalField(max_digits=5, decimal_places=2)
-    loweststore = models.CharField(max_length=10)
-    lowesturl = models.URLField(max_length=200)
-    historiclow = models.DecimalField(max_digits=5, decimal_places=2)
+    walmartprice = models.DecimalField(max_digits=5, decimal_places=2, default=100.00)
+    lowestprice = models.DecimalField(max_digits=5, decimal_places=2, default=100.00)
+    loweststore = models.CharField(max_length=10, default='None')
+    lowesturl = models.URLField(max_length=200, default='amazon.com')
+    historiclow = models.DecimalField(max_digits=5, decimal_places=2, default=100.00)
 
     def __str__(self):
         return self.title
